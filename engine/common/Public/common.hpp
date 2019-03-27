b54e4a698fe887263a6db72d476be7e0fdf28325
bomkvilt <kvilt-d.max@yandex.ru>
2019-03-27 18:08:54 +0300

    [modules] common

---
+++ engine/common/Public/common.hpp
@@ -0,0 +1,19 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <memory>
+
+// ------| macro
+#include "macro.hpp"
+
+// ------| math
+#include "math/math.hpp"
+#include "math/math_common.hpp"
+#include "math/color.hpp"
+#include "math/quat.hpp"
+#include "math/vector.hpp"
+#include "math/vector2.hpp"
+#include "math/transform.hpp"
+
+
+#endif //!COMMON_HPP
---
+++ engine/common/Public/macro.hpp
@@ -0,0 +1,8 @@
+#ifndef MACRO_HPP
+#define MACRO_HPP
+
+#define UNIQUE(CLASS) std::unique_ptr<CLASS>
+#define SHARED(CLASS) std::shared_ptr<CLASS>
+
+
+#endif //!MACRO_HPP
